2f00168fd711dc910695da34e5ab6482a981fd8f
Kevin Crook <kevinwaynecrook@gmail.com>
2024-04-30 21:52:15 -0600

    Reorganise: move modules to common

--- nixosConfigurations/krypton/default.nix
+++ nixosConfigurations/krypton/default.nix
@@ -4,12 +4,10 @@
   modules = [
     ./configuration.nix
     ./hardware-configuration.nix
-    inputs.self.nixosModules.boot
     inputs.self.nixosModules.common
 #    inputs.self.nixosModules.desktop
-    inputs.self.nixosModules.intelgpu
+#    inputs.self.nixosModules.intelgpu
     inputs.self.nixosModules.libvirt
-    inputs.self.nixosModules.network
     inputs.self.nixosModules.podman
   ];
 }
--- nixosModules/common.nix
+++ nixosModules/common.nix
@@ -1,7 +1,9 @@
 { inputs, config, pkgs, lib, ... }: {
   imports = [
+    inputs.self.nixosModules.boot
     inputs.self.nixosModules.locale
     inputs.self.nixosModules.neovim
+    inputs.self.nixosModules.network
     inputs.self.nixosModules.nix
     inputs.self.nixosModules.user-kcrook
   ];
