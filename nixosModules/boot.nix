a6b22ef9548906d9f9ade04783c91825fdcc3d1d
Kevin Crook <kevinwaynecrook@gmail.com>
2024-04-30 21:15:31 -0600

    Add: move splash related settings to bootsplash

--- nixosModules/boot.nix
+++ nixosModules/boot.nix
@@ -1,16 +1,5 @@
 { config, pkgs, lib, ... }: {
-  console = { earlySetup = true; };
-
   boot = {
     blacklistedKernelModules = [ "snd_pcsp" ];
-    plymouth.enable = true;
-
-    kernelParams = [
-      "quiet"
-      "splash"
-      "loglevel=3"
-      "rd.udev.log_priority=3"
-      "vt.global_cursor_default=0"
-    ];
   };
 }
---
+++ nixosModules/bootsplash.nix
@@ -0,0 +1,15 @@
+{ config, pkgs, lib, ... }: {
+  console = { earlySetup = true; };
+
+  boot = {
+    plymouth.enable = true;
+
+    kernelParams = [
+      "quiet"
+      "splash"
+      "loglevel=3"
+      "rd.udev.log_priority=3"
+      "vt.global_cursor_default=0"
+    ];
+  };
+}
