8569e98c8e2e7c0cb9d118ed9144e670ea11a983
Kevin Crook <kevinwaynecrook@gmail.com>
2024-05-11 04:12:05 -0600

    add: prepare for creation of host zinc

---
+++ nixosConfigurations/zinc/configuration.nix
@@ -0,0 +1,29 @@
+{ inputs, config, lib, pkgs, ... }: {
+  boot = {
+    kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
+    loader.efi.canTouchEfiVariables = true;
+    supportedFilesystems = [ "zfs" ];
+    loader.grub = {
+      enable = true;
+      efiSupport = true;
+      zfsSupport = true;
+    };
+  };
+
+  networking = {
+    hostName = "zinc";
+    hostId = "983ee865";
+    firewall.enable = false;
+    bridges.br0.interfaces = [ "enp2s0" ];
+    interfaces.br0.useDHCP = true;
+    useDHCP = false;
+  };
+
+  services.zfs = {
+    autoScrub.enable = true;
+    trim.enable = true;
+  };
+
+  system.stateVersion = "23.11";
+}
+
