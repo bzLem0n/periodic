47c56cd62a0e5ee1d68b2b3e5dcee71cf7ea82f5
Kevin Crook <kevinwaynecrook@gmail.com>
2024-05-11 07:12:36 +0000

    add: reinstall krypton of zfs filesystem

--- nixosConfigurations/krypton/hardware-configuration.nix
+++ nixosConfigurations/krypton/hardware-configuration.nix
@@ -22,6 +22,5 @@
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode =
     lib.mkDefault config.hardware.enableRedistributableFirmware;
-  boot.swraid.enable = true;
 
 }
