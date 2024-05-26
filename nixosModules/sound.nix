fb8c5f5d0933b49924ba591754512ea47d4676b0
Kevin Crook <kevinwaynecrook@gmail.com>
2024-05-26 15:04:19 -0600

    add: define low-latency pipewire-pulse options with values matching pipewire

--- nixosModules/sound.nix
+++ nixosModules/sound.nix
@@ -12,13 +12,32 @@
       support32Bit = true;
     };
 
-    extraConfig.pipewire."91-low-latency" = {
-      context.properties = {
-        default.clock.rate = 48000;
-        default.clock.quantum = 32;
-        default.clock.min-quantum = 32;
-        default.clock.max-quantum = 32;
+    extraConfig = {
+      pipewire."91-low-latency" = {
+        context.properties = {
+          default.clock.rate = 48000;
+          default.clock.quantum = 32;
+          default.clock.min-quantum = 32;
+          default.clock.max-quantum = 32;
+        };
       };
+      pipewire-pulse."92-low-latency" = {
+        context.modules = [{
+          name = "libpipewire-module-protocol-pulse";
+          args = {
+            pulse.min.req = "32/48000";
+            pulse.default.req = "32/48000";
+            pulse.max.req = "32/48000";
+            pulse.min.quantum = "32/48000";
+            pulse.max.quantum = "32/48000";
+          };
+        }];
+        stream.properties = {
+          node.latency = "32/48000";
+          resample.quality = 1;
+        };
+      };
+
     };
 
     wireplumber = {
