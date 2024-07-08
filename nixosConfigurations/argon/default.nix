21eb9a78f7d9aee9e63113a5724bd0e02bfb4603
Kevin Crook <kevinwaynecrook@gmail.com>
2024-07-07 19:53:46 -0600

    remove: remove nix cache server

--- nixosConfigurations/argon/default.nix
+++ nixosConfigurations/argon/default.nix
@@ -5,7 +5,6 @@
     inputs.self.nixosModules.common
     inputs.self.nixosModules.desktop
     inputs.self.nixosModules.intelgpu
-    inputs.self.nixosModules.nixcache
 
     inputs.home-manager.nixosModules.home-manager
     inputs.disko.nixosModules.disko
