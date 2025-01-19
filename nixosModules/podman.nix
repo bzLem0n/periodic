a05e0385dbecf966d50717a9f6a626894c6ce44e
Kevin Crook <kevinwaynecrook@gmail.com>
2025-01-19 17:44:12 -0600

    add: add usefull software for podman

--- nixosModules/podman.nix
+++ nixosModules/podman.nix
@@ -1,10 +1,18 @@
 { config, pkgs, lib, ... }: {
   virtualisation = {
+    containers.enable = true;
     oci-containers.backend = "podman";
     podman = {
       enable = true;
+      autoPrune.enable = true;
       defaultNetwork.settings.dns_enabled = true;
       dockerCompat = true;
     };
   };
+  environment.systemPackages = with pkgs; [
+    dive # look into docker image layers
+    podman
+    podman-tui # Terminal mgmt UI for Podman
+    passt # For Pasta rootless networking
+  ];
 }
