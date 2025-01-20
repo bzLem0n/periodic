1a19b726df92377a1beaff471db2e3d76c60d7e0
Kevin Crook <kevinwaynecrook@gmail.com>
2025-01-19 20:04:12 -0600

    add: docker-compose for users

--- nixosModules/podman.nix
+++ nixosModules/podman.nix
@@ -1,6 +1,16 @@
 { config, pkgs, lib, ... }: {
   virtualisation = {
     containers.enable = true;
+    containers.storage.settings = {
+      storage = {
+        driver = "overlay";
+        runroot = "/run/containers/storage";
+        graphroot = "/var/lib/containers/storage";
+        rootless_storage_path = "/tmp/containers-$USER";
+        options.overlay.mountopt = "nodev,metacopy=on";
+      };
+    };
+
     oci-containers.backend = "podman";
     podman = {
       enable = true;
@@ -9,10 +19,18 @@
       dockerCompat = true;
     };
   };
+
   environment.systemPackages = with pkgs; [
+    docker-compose
     dive # look into docker image layers
     podman
     podman-tui # Terminal mgmt UI for Podman
     passt # For Pasta rootless networking
   ];
+
+  environment.extraInit = ''
+    if [ -z "$DOCKER_HOST" -a -n "$XDG_RUNTIME_DIR" ]; then
+      export DOCKER_HOST="unix://$XDG_RUNTIME_DIR/podman/podman.sock"
+    fi
+  '';
 }
