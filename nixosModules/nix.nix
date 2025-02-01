7da74fe9cfef98a875753e37b646d23088cc5c02
Kevin Crook <kevinwaynecrook@gmail.com>
2025-01-31 21:53:12 -0600

    add: change caches to extras instead of main

--- nixosModules/nix.nix
+++ nixosModules/nix.nix
@@ -15,15 +15,13 @@
   nix = {
     settings.experimental-features = [ "nix-command" "flakes" ];
     settings = {
-      substituters = [
+      extra-substituters = [
         "http://192.168.1.36"
         "https://nix-community.cachix.org/"
-        "https://cache.nixos.org/"
       ];
-      trusted-public-keys = [
+      extra-trusted-public-keys = [
         "192.168.1.36:tEdLEHu8aGnsnr/XL4uulyytFZjS6QTtASO49Z0af28="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       ];
       trusted-users = [ "root" "@wheel" ];
     };
