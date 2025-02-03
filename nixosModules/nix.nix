04abe48773b1bf1f3f50e146fc5a8a9cae824ea4
Kevin Crook <kevinwaynecrook@gmail.com>
2025-02-03 17:03:07 -0600

    Revert "add: change caches to extras instead of main"
    This reverts commit 7da74fe9cfef98a875753e37b646d23088cc5c02.

--- nixosModules/nix.nix
+++ nixosModules/nix.nix
@@ -15,13 +15,15 @@
   nix = {
     settings.experimental-features = [ "nix-command" "flakes" ];
     settings = {
-      extra-substituters = [
+      substituters = [
         "http://192.168.1.36"
         "https://nix-community.cachix.org/"
+        "https://cache.nixos.org/"
       ];
-      extra-trusted-public-keys = [
+      trusted-public-keys = [
         "192.168.1.36:tEdLEHu8aGnsnr/XL4uulyytFZjS6QTtASO49Z0af28="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       ];
       trusted-users = [ "root" "@wheel" ];
     };
