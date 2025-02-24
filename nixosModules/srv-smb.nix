3c174e411ca6b52266a59925461dc7dc013374c1
Kevin Crook <kevinwaynecrook@gmail.com>
2025-02-23 20:55:09 -0600

    fix: use wiki recommended settings

--- nixosModules/srv-smb.nix
+++ nixosModules/srv-smb.nix
@@ -5,13 +5,10 @@
     settings = {
       global = {
         "workgroup" = "PERIODIC";
-        "server string" = "xenon";
+        "server string" = "Samba Server";
         "netbios name" = "xenon";
         "security" = "user";
-        #"use sendfile" = "yes";
-        #"max protocol" = "smb2";
-        # note: localhost is the ipv6 localhost ::1
-        "hosts allow" = "192.168.0. 127.0.0.1 localhost";
+        "hosts allow" = "192.168.1. 127.0.0.1 localhost";
         "hosts deny" = "0.0.0.0/0";
         "guest account" = "nobody";
         "map to guest" = "bad user";
@@ -23,17 +20,9 @@
         "guest ok" = "yes";
         "create mask" = "0644";
         "directory mask" = "0755";
+        "force user" = "kcrook";
+        "force group" = "users";
       };
-      # "private" = {
-      #   "path" = "/mnt/Shares/Private";
-      #   "browseable" = "yes";
-      #   "read only" = "no";
-      #   "guest ok" = "no";
-      #   "create mask" = "0644";
-      #   "directory mask" = "0755";
-      #   "force user" = "username";
-      #   "force group" = "groupname";
-      # };
     };
   };
 
@@ -45,9 +34,7 @@
   services.avahi = {
     publish.enable = true;
     publish.userServices = true;
-    # ^^ Needed to allow samba to automatically register mDNS records (without the need for an `extraServiceFile`
     nssmdns4 = true;
-    # ^^ Not one hundred percent sure if this is needed- if it aint broke, don't fix it
     enable = true;
     openFirewall = true;
   };
