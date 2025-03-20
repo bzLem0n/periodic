21eefc3cca08627fadb762bf8ee398862ca51346
Kevin Crook <kevinwaynecrook@gmail.com>
2025-03-19 20:41:44 -0600

    add: specify wallpaper url

--- homeModules/theme.nix
+++ homeModules/theme.nix
@@ -1,6 +1,10 @@
 { inputs, pkgs, ... }: {
   stylix = {
     enable = true;
+    image = pkgs.fetchurl {
+      url = "https://w.wallhaven.cc/full/o5/wallhaven-o5ky29.png";
+      hash = "sha256-M4Q5/Sm5ZVOe324V9pEF9497M0VpcQMPTJHOwW2ZQmg=";
+    };
     base16Scheme = "${pkgs.base16-schemes}/share/themes/selenized-black.yaml";
     cursor = {
       package = pkgs.kdePackages.breeze;
