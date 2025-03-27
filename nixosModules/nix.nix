ba5933a4076126530d11610abb729673e5efe1cb
Kevin Crook <kevinwaynecrook@gmail.com>
2025-03-26 19:05:57 -0600

    Revert "refactor: Move substituters to flake.nix"
    This reverts commit 522102125fb0c7636e58c00c263174541bfbc795.

--- nixosModules/nix.nix
+++ nixosModules/nix.nix
@@ -11,4 +11,21 @@
     };
     flake = "/home/kcrook/code/github/bzLem0n/periodic";
   };
+
+  nix = {
+    settings.experimental-features = [ "nix-command" "flakes" ];
+    settings = {
+      substituters = [
+        "http://192.168.1.36/"
+        "https://nix-community.cachix.org/"
+        "https://cache.nixos.org/"
+      ];
+      trusted-public-keys = [
+        "192.168.1.36-1:jecpDmt3fNblbGi/8ZwpxsY2z8rs9BDs6ZMaNs9LrGk="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      ];
+      trusted-users = [ "root" "@wheel" ];
+    };
+  };
 }
