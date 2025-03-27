3280f41df17b217496dcd6314c9faf0a25bf1daa
Kevin Crook <kevinwaynecrook@gmail.com>
2025-03-26 19:09:52 -0600

    fix: Re-add nix settings in flake.nix
    The nix options set in nixosModules/nix are
    needed for the declared systems.  The options in
    flake.nix are only for when working with the
    flake.

--- flake.nix
+++ flake.nix
@@ -42,6 +42,21 @@
     systems.url = "github:nix-systems/x86_64-linux";
   };
 
+  nixConfig = {
+    experimental-features = [ "nix-command" "flakes" ];
+    extra-substituters = [
+      "http://192.168.1.36/"
+      "https://nix-community.cachix.org/"
+      "https://cache.nixos.org/"
+    ];
+    extra-trusted-public-keys = [
+      "192.168.1.36-1:jecpDmt3fNblbGi/8ZwpxsY2z8rs9BDs6ZMaNs9LrGk="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    ];
+    trusted-users = [ "root" "@wheel" ];
+  };
+
   outputs = { flakelight, home-manager, disko, agenix, nix-index-database, stylix, systems, ... }@inputs:
     flakelight ./. {
       inherit inputs;
