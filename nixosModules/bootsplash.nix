c0a04bcea1ef4148397a2fe1ea37e5741f2b6241
Kevin Crook <kevinwaynecrook@gmail.com>
2025-04-22 20:00:21 -0600

    feat: Update bootsplash settings
    Update plymouth and boot splash options.

--- nixosModules/bootsplash.nix
+++ nixosModules/bootsplash.nix
@@ -2,13 +2,20 @@
   console.earlySetup = true;
 
   boot = {
-    plymouth.enable = true;
+    plymouth = {
+      enable = true;
+      font = "${pkgs.hack-font}/share/fonts/truetype/Hack-Regular.ttf";
+      logo = "${pkgs.nixos-icons}/share/icons/hicolor/128x128/apps/nix-snowflake.png";
+    };
+
+    consoleLogLevel = 3;
+    initrd.verbose = false;
 
     kernelParams = [
       "quiet"
       "splash"
-      "loglevel=3"
-      "rd.udev.log_priority=3"
+      "udev.log_priority=3"
+      "rd.systemd.show_status=auto"
       "vt.global_cursor_default=0"
     ];
   };
