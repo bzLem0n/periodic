3b714034f62488b8a57abd72d8d360a5079412e9
Kevin Crook <kevinwaynecrook@gmail.com>
2025-07-23 21:18:38 -0600

    feat(bootsplash): Enable seamless boot splash
    Create a seamless transition between plymouth and sddm.  Taken from:
    https://discourse.nixos.org/t/smooth-transition-with-plymouth-sddm-kde/4646/2

--- nix/nixosModules/bootsplash.nix
+++ nix/nixosModules/bootsplash.nix
@@ -25,4 +25,12 @@
       "vt.global_cursor_default=0"
     ];
   };
+
+  systemd.services."display-manager" = {
+    conflicts = [ "plymouth-quit.service" ];
+    preStart = "${pkgs.plymouth}/bin/plymouth deactivate";
+    script = "/run/current-system/sw/bin/sddm";
+    postStart = "/bin/sh -c 'sleep 5 && ${pkgs.plymouth}/bin/plymouth quit --retain-splash'";
+    enable = true;
+  };
 }
