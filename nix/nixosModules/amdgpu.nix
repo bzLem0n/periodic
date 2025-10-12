de25008f22fd86c276040da903cd329f7a6bafdd
Kevin Crook <kevinwaynecrook@gmail.com>
2025-10-11 23:31:15 -0600

    fix(amdgpu): amdvlk is discontinued

--- nix/nixosModules/amdgpu.nix
+++ nix/nixosModules/amdgpu.nix
@@ -6,9 +6,4 @@
 }:
 {
   boot.initrd.availableKernelModules = [ "amdgpu" ];
-
-  hardware.graphics = {
-    extraPackages = [ pkgs.amdvlk ];
-    extraPackages32 = [ pkgs.driversi686Linux.amdvlk ];
-  };
 }
