3a3bcdc6f453569a4f1eda9879ec85129b71a1f1
Kevin Crook <kevinwaynecrook@gmail.com>
2026-05-10 00:17:51 -0600

    feat(module): Configure Helix

--- nix/homeModules/helix.nix
+++ nix/homeModules/helix.nix
@@ -8,25 +8,64 @@
   programs.helix = {
     enable = true;
     settings = {
-      theme = "autumn_night_transparent";
-      editor.cursor-shape = {
-        normal = "block";
-        insert = "bar";
-        select = "underline";
+      theme = "base16_transparent";
+      editor = {
+        bufferline = "multiple";
+        cursorline = true;
+        line-number = "relative";
+        rulers = [ 120 ];
+        true-color = true;
+        completion-replace = true;
+        trim-trailing-whitespace = true;
+        end-of-line-diagnostics = true;
+        color-modes = true;
+        soft-wrap.enable = true;
+        cursor-shape = {
+          normal = "block";
+          insert = "bar";
+          select = "underline";
+        };
+        indent-guides = {
+          character = "╎";
+          render = true;
+        };
+        lsp = {
+          auto-signature-help = false;
+          display-messages = true;
+        };
+        statusline = {
+          left = [
+            "mode"
+            "spinner"
+            "version-control"
+            "spacer"
+            "separator"
+            "file-name"
+            "read-only-indicator"
+            "file-modification-indicator"
+          ];
+          center = [ ];
+          right = [
+            "diagnostics"
+            "workspace-diagnostics"
+            "position"
+            "total-line-numbers"
+            "position-percentage"
+            "file-encoding"
+            "file-line-ending"
+            "file-type"
+            "register"
+            "selections"
+          ];
+        };
       };
     };
     languages.language = [
       {
         name = "nix";
         auto-format = true;
-        formatter.command = lib.getExe pkgs.nixfmt-rfc-style;
+        formatter.command = lib.getExe pkgs.nixfmt-rs;
       }
     ];
-    themes = {
-      autumn_night_transparent = {
-        "inherits" = "autumn_night";
-        "ui.background" = { };
-      };
-    };
   };
 }
