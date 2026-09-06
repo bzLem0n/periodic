c6726dcc9f3f67d78227ea8faaeecc38282dce71
Kevin Crook <kevinwaynecrook@gmail.com>
2026-09-05 22:25:18 -0600

    fix(inputs): xz has been depreciated for zst

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     #nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
+    nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.zst";
 
     flakelight = {
       url = "github:nix-community/flakelight";
